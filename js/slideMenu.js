842370baea174d64529e687a8971acad1b4973b5
Maria Gargoles <mariagargoles@live.com>
2024-06-19 09:52:06 +0200

    change js slide menu

--- js/slideMenu.js
+++ js/slideMenu.js
@@ -1,15 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const swiper = new Swiper(".swiper-container", {
-    direction: "vertical",
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    slidesPerView: 3,
-  });
+const swiper = new Swiper(".swiper-container", {
+  direction: "vertical",
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 3,
 });
