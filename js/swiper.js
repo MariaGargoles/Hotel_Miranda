1ffb423cb3927ceecbbb5515496b895175365ccf
Maria Gargoles <mariagargoles@live.com>
2024-06-20 22:36:15 +0200

    Finish Facilities, problem with swiper

--- js/swiper.js
+++ js/swiper.js
@@ -1,9 +1,18 @@
-const swiper = new Swiper(".swiper--menu", {
+const swiperMenu = new Swiper(".swiper--menu", {
   direction: "horizontal",
   loop: true,
 
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+});
+
+const swiperFacilities = new Swiper(".swiper--facilities", {
+  direction: "horizontal",
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
 });
